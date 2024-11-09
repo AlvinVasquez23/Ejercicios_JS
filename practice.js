32367a9ca01f130654af07f0da457f15b93935a9
Your Name <you@example.com>
2024-11-09 12:35:22 -0500

    cambio menor

--- practice.js
+++ practice.js
@@ -535,13 +535,7 @@ const arrayAlCuadrado = (lista) =>{
     return `La lista elevada al cuadrado es la siguiente: ${nuevaLista}`;
 };
 
-console.log(arrayAlCuadrado());
-console.log(arrayAlCuadrado(1545));
-console.log(arrayAlCuadrado("hola"));
-console.log(arrayAlCuadrado([]));
-console.log(arrayAlCuadrado([5]));
-console.log(arrayAlCuadrado([5,10,15]));
-console.log(arrayAlCuadrado([5,10,'15']));
+/* console.log(arrayAlCuadrado()); */
 
 /*22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}. */
